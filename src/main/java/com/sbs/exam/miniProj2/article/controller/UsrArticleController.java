8ae5f020a361329f4e6fd09f836e5fa3f921614d
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-06 17:31:04 +0900

    "writeArticle 메서드를 통해서 게시물 작성로직 중복 제거"

--- src/main/java/com/sbs/exam/miniProj2/article/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/miniProj2/article/controller/UsrArticleController.java
@@ -25,20 +25,16 @@ public class UsrArticleController {
 
     private void makeTestDate() {
         for(int i = 1; i <= 10; i++){
-            int id = articleLastId + 1;
+
             String title = "제목" + i;
             String body = "내용" + i;
-            Article article = new Article(id, title, body);
 
-            articles.add(article);
-            articleLastId = id;
-
+            writeArticle(title, body);
         }
     }
 
-    @RequestMapping("/usr/article/doAdd")
-    @ResponseBody
-    public Article doAdd(String title, String body) {
+
+    private Article writeArticle(String title, String body) {
         int id = articleLastId + 1;
         Article article = new Article(id, title, body);
 
@@ -47,6 +43,15 @@ public class UsrArticleController {
 
         return article;
     }
+
+    @RequestMapping("/usr/article/doAdd")
+    @ResponseBody
+    public Article doAdd(String title, String body) {
+       Article article = writeArticle(title, body);
+
+        return article;
+    }
+
 
     @RequestMapping("/usr/article/getArticles")
     @ResponseBody
