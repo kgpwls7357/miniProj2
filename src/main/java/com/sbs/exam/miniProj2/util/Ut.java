aca98bd60dfd39b02ebc3492c627b56bcdf35774
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-07 00:45:15 +0900

    "Ut.f 함수 도입으로 복잡한 문장 구성을 편하게"

--- src/main/java/com/sbs/exam/miniProj2/util/Ut.java
+++ src/main/java/com/sbs/exam/miniProj2/util/Ut.java
@@ -14,4 +14,7 @@ public class Ut {
 
         return str.trim().length() == 0;
     }
+    public static String f(String format, Object... args) {
+        return String.format(format, args);
+    }
 }
