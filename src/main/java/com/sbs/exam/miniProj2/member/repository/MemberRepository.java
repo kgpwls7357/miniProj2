a0bd5c0f3e5650900dfcf60e934b3ae7e322893d
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-06 22:57:42 +0900

    "MemberRepository 추가, 회원가입 처리"

--- src/main/java/com/sbs/exam/miniProj2/member/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/miniProj2/member/repository/MemberRepository.java
@@ -1,5 +1,22 @@
 package com.sbs.exam.miniProj2.member.repository;
 
-public class MemberRepository {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+@Mapper
+public interface MemberRepository {
+    @Insert("""
+          INSERT INTO `member`
+          SET regDate = NOW(),
+          updateDate = NOW(),
+          loginId = #{loginId},
+          loginPw = #{loginPw},
+          `name` = #{name},
+          `nickname` = #{nickname},
+          cellphoneNo = #{cellphoneNo},
+          email = #{email}
+          """)
+    public void join(@Param("loginId") String loginId, @Param("loginPw") String loginPw, @Param("name") String name,
+                     @Param("nickname") String nickname, @Param("cellphoneNo") String cellphoneNo, @Param("email") String email);
 }
--- src/main/java/com/sbs/exam/miniProj2/member/service/MemberService.java
+++ src/main/java/com/sbs/exam/miniProj2/member/service/MemberService.java
@@ -1,13 +1,17 @@
 package com.sbs.exam.miniProj2.member.service;
 
+import com.sbs.exam.miniProj2.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class MemberService {
-    // private MemberRepository memberRepository;
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-        // memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+        memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
     }
 }
