1f0eb83664a4aced3bb906c8654d56d7c4414d12
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-06 17:21:49 +0900

    "게시물 컨트롤러에, 게시물 추가, 리스트 기능 구현"

---
+++ src/main/java/com/sbs/exam/miniProj2/article/vo/Article.java
@@ -0,0 +1,15 @@
+package com.sbs.exam.miniProj2.article.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Article {
+        private int id;
+        private String title;
+        private String body;
+
+    }
