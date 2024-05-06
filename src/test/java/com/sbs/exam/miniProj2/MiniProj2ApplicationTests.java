3ec763dc20324f0aa0f1de907c22b075a90e0067
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-06 14:12:38 +0900

    환경설정(application.yml 설정)

---
+++ src/test/java/com/sbs/exam/miniProj2/MiniProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.miniProj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
